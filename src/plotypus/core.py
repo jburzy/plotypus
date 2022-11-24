2d79570fd1f2884d1c15e358d00171a9e901f6c3
jburzy <jackson.carl.burzynski@cern.ch>
2022-11-24 15:18:22 -0800

    clean up style

--- src/plotypus/core.py
+++ src/plotypus/core.py
@@ -4,35 +4,32 @@ import ROOT
 def get_x_label(plot: dict) -> str:
 
     label = plot['x_label']
-
     if plot.get('units'):
-        label += f" {plot.get['units']}"
-
+        label += f" [{plot.get('units')}]"
     return label
 
 def get_y_label(plot: dict, bin_width: float = None) -> str:
 
     label = plot.get('y_label',"Events")
-
     if plot.get('units'):
-        label += f" / {bin_width} {plot.get['units']}"
-
+        label += f" / {bin_width} {plot.get('units')}"
     return label
 
 def make_plot(plot: dict) -> None:
     # Set the ATLAS Style
     aplt.set_atlas_style()
 
-    ratio = plot.get('ratio')
+    plot_style = plot['style']
+    ratio = plot_style.get('ratio')
 
     numerator = None
     denominator = None
 
     # Create a figure and axes
     if ratio:
-        fig, (ax1, ax2) = aplt.ratio_plot(name="fig1", figsize=(800, 800), hspace=0.10)
+        fig, (ax1, ax2) = aplt.ratio_plot(name=plot['name'], figsize=(800, 800), hspace=0.10)
     else:
-        fig, ax1 = aplt.subplots(1, 1, name="fig1", figsize=(800, 600))
+        fig, ax1 = aplt.subplots(1, 1, name=plot['name'], figsize=(800, 600))
 
     # create legend
     legend = ax1.legend(
@@ -48,8 +45,8 @@ def make_plot(plot: dict) -> None:
         tfiles.append(tfile)
         obj = tfile.Get(plot['path'])
 
-        if plot.get('rebin'):
-            rebin = plot['rebin']
+        if plot_style.get('rebin'):
+            rebin = plot_style['rebin']
             if isinstance(rebin, list):
                 import array
                 xbins = array.array('d', rebin)
@@ -60,10 +57,10 @@ def make_plot(plot: dict) -> None:
         if sample.get('is_data'):
             obj.SetBinErrorOption(ROOT.TH1.EBinErrorOpt.kPoisson)
             obj_graph = aplt.root_helpers.hist_to_graph(obj)
-            ax1.plot(obj_graph, 'P')
+            ax1.plot(obj_graph, 'P', **sample['style'])
             legend.AddEntry(obj_graph, sample['name'], sample['legend_format'])
         else:
-            ax1.plot(obj, sample['draw_style'], label=sample['legend'], labelfmt=sample['legend_format'], **sample['style'])
+            ax1.plot(obj, sample['draw_style'], **sample['style'])
             legend.AddEntry(obj, sample['name'], sample['legend_format'])
 
         if sample.get('numerator'):
@@ -74,42 +71,44 @@ def make_plot(plot: dict) -> None:
     if ratio and (not numerator or not denominator):
         raise RuntimeError("Ratio requested but no numerator or denominator specified. Aborting")
 
-    if plot.get('log_scale_y'):
+    if plot_style.get('log_scale_y'):
         ax1.set_yscale("log") 
 
     # Set axis titles
-    (ax2 if ratio else ax1).set_xlabel(get_x_label(plot))
-    ax1.set_ylabel(get_y_label(plot, obj.GetBinWidth(1)))
+    print(get_x_label(plot_style))
+    (ax2 if ratio else ax1).set_xlabel(get_x_label(plot_style), titleoffset=1.3)
+    ax1.set_ylabel(get_y_label(plot_style, obj.GetBinWidth(1)), maxdigits=3)
 
     # set the main axis limits
-    ax1.set_xlim(plot.get('x_min'), plot.get('x_max'))
-    ax1.set_ylim(plot.get('y_min'), plot.get('y_max'))
+    ax1.set_xlim(plot_style.get('x_min'), plot_style.get('x_max'))
+    ax1.set_ylim(plot_style.get('y_min'), plot_style.get('y_max'))
     
     if ratio:
         # Calculate and draw the ratio
         ratio_hist = numerator.Clone("ratio_hist")
         ratio_hist.Divide(denominator)
         ratio_graph = aplt.root_helpers.hist_to_graph(ratio_hist)
-        ax2.plot(ratio_graph, "P0")
+        ax2.plot(ratio_graph, "P0", linewidth=2)
 
         # Draw line at y=1 in ratio panel
-        line = ROOT.TLine(ax1.get_xlim()[0], plot.get('ratio_line',1), ax1.get_xlim()[1], plot.get('ratio_line',1))
+        line = ROOT.TLine(ax1.get_xlim()[0], plot_style.get('ratio_line',1), ax1.get_xlim()[1], plot_style.get('ratio_line',1))
+        line.SetLineStyle(2)
         ax2.plot(line)
 
         ax2.set_xlim(ax1.get_xlim())
-        ax2.set_ylim(plot.get('ratio_min',0.75), plot.get('ratio_min',1.25))
-        ax2.set_ylabel(plot.get('ratio_label','Data/MC'), loc="centre")
+        ax2.set_ylim(plot_style.get('ratio_min',0.75), plot_style.get('ratio_min',1.25))
+        ax2.set_ylabel(plot_style.get('ratio_label','Data/MC'), loc="centre")
 
-        if plot.get('draw_arrows'):
+        if plot_style.get('draw_arrows'):
             ax2.draw_arrows_outside_range(ratio_graph)
 
     # Go back to top axes to add labels
     ax1.cd()
     # Add extra space at top of plot to make room for labels
-    ax1.add_margins(top=plot.get('pad_top'))
+    ax1.add_margins(top=plot_style.get('pad_top',0.20))
 
     # Add the ATLAS Label
     aplt.atlas_label(text="Internal", loc="upper left")
-    ax1.text(0.2, 0.84, "#sqrt{s} = 13 TeV, 139 fb^{-1}", size=22, align=13)
+    ax1.text(0.2, 0.85, "#sqrt{s} = 13 TeV, 139 fb^{-1}", size=22, align=13)
 
-    fig.savefig("test.pdf")
+    fig.savefig(f"{plot['name']}.pdf")
